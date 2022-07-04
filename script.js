2df77609881df1982874d5b740725335c5fdbf29
Namik02 <Hasannamik02@outlook.com>
2022-07-04 06:48:21 -0700

    playRound function

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 function computerSelection() {
     const options = ["Rock", "Paper", "Scissors"]
-    let cInput = options[Math.floor(Math.random() * 3)]
-    cInput.toLowerCase
+    const cInput = options[Math.floor(Math.random() * 3)]
     return cInput
 }
 
 function playerSelection() {
     let pInput = prompt("Play by entering Rock/Paper/Scissors")
-    pInput.toLowerCase
-    return pInput
+    const pInput1 = pInput.substring(0,1).toUpperCase() + pInput.substring(1).toLowerCase()
+    return pInput1
+}
+
+function playRound(computerSelection,playerSelection) {
+    if (computerSelection==="Rock" && playerSelection==="Rock"
+      ||computerSelection==="Paper" && playerSelection==="Paper"
+      ||computerSelection==="Scissors" && playerSelection==="Scissors") {
+        return "You Drew!"
+    } else if (computerSelection==="Rock" && playerSelection==="Paper") {
+        return "You Win! Paper beats Rock"
+    } else if (computerSelection==="Rock" && playerSelection==="Scissors") {
+        return "You Lose! Scissors beats Rock"
+    } else if (computerSelection==="Paper" && playerSelection==="Rock") {
+        return "You Lose! Paper beats Rock"
+    } else if (computerSelection==="Paper" && playerSelection==="Scissors") {
+        return "You Win! Scissors beats Paper"
+    } else if (computerSelection==="Scissors" && playerSelection==="Rock") {
+        return "You Win! Rock beats Scissors"
+    } else if (computerSelection==="Scissors" && playerSelection==="Paper") {
+        return "You Lose! Scissors beats Paper"
+    } else {
+        return "You Entered A Incorrect Value"
+    }
+
 }
 
 
 
+const cs = computerSelection()
+const ps = playerSelection()
+console.log("The Computer Chose " + cs + " And You Chose " + ps)
+console.log(playRound(cs,ps))
